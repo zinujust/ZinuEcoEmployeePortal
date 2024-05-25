3400ed6dc14b522e74db21cbcee8fcd51f9a537d
Zainal Shariff <zainalshariff@Zainals-MacBook-Pro.local>
2024-05-24 21:24:39 -0700

    Created eureka server microservice for project

---
+++ employee-portal-server/src/main/java/app/zinu/employee_portal_server/EmployeePortalServerApplication.java
@@ -0,0 +1,13 @@
+package app.zinu.employee_portal_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePortalServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePortalServerApplication.class, args);
+	}
+
+}
---
+++ employee-portal-server/src/test/java/app/zinu/employee_portal_server/EmployeePortalServerApplicationTests.java
@@ -0,0 +1,13 @@
+package app.zinu.employee_portal_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePortalServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
